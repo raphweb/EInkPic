472977695ecadce521b189fd66e81682473c0c8f
Raphael Weber <5662800+raphweb@users.noreply.github.com>
2023-06-25 23:48:35 +0200

    Tried to make 7.3inch 7c EPD work.

--- software/src/DisplayConfig.hpp
+++ software/src/DisplayConfig.hpp
@@ -1,24 +1,34 @@
-#define GxEPD2_DISPLAY_CLASS GxEPD2_BW
-#define GxEPD2_DRIVER_CLASS GxEPD2_270
-// pin definitions
-#ifdef ESP32
-// this is the VSPI on the ESP32
-#define EPD_CS 5
-#define EPD_DC 19
-#define EPD_RST 22
-#define EPD_BUSY 21
-#endif
+#ifndef DISPLAY_CONFIG_HPP_
+#define DISPLAY_CONFIG_HPP_
 
-#ifdef ESP8266
-#define EPD_CS D8
-#define EPD_DC D3
-#define EPD_RST D1
-#define EPD_BUSY D4
-#endif
+#include <GxEPD2_7C.h>
 
+#define GxEPD2_DISPLAY_CLASS GxEPD2_7C
+#define GxEPD2_DRIVER_CLASS GxEPD2_730c
+
+// Connections for e.g. LOLIN D32
+#define EPD_BUSY  4 // to EPD BUSY
+#define EPD_CS    5 // to EPD CS
+#define EPD_RST  16 // to EPD RST
+#define EPD_DC   17 // to EPD DC
+#define EPD_SCK  18 // to EPD CLK
+#define EPD_MISO 19 // Master-In Slave-Out not used, as no data from display
+#define EPD_MOSI 23 // to EPD DIN
 
 #define MAX_DISPLAY_BUFFER_SIZE 65536UL
 #define MAX_HEIGHT(EPD) (EPD::HEIGHT <= (MAX_DISPLAY_BUFFER_SIZE) / (EPD::WIDTH / 2) ? EPD::HEIGHT : (MAX_DISPLAY_BUFFER_SIZE) / (EPD::WIDTH / 2))
 GxEPD2_DISPLAY_CLASS<GxEPD2_DRIVER_CLASS, MAX_HEIGHT(GxEPD2_DRIVER_CLASS)> display(GxEPD2_DRIVER_CLASS(EPD_CS, EPD_DC, EPD_RST, EPD_BUSY));
 #undef MAX_DISPLAY_BUFFER_SIZE
 #undef MAX_HEIGHT
+
+SPIClass hspi(HSPI);
+
+void initialiseDisplay() {
+    hspi.begin(EPD_SCK, EPD_MISO, EPD_MOSI, EPD_CS);
+    display.epd2.selectSPI(hspi, SPISettings(2500000, MSBFIRST, SPI_MODE0));
+    display.init(115200);//, true, 2, false);
+    display.clearScreen();
+    display.setFullWindow();
+}
+
+#endif
--- software/src/EInkPic.cpp
+++ software/src/EInkPic.cpp
@@ -3,25 +3,26 @@
 #include <GxEPD2_BW.h>
 #include <Fonts/FreeSans12pt7b.h>
 
-#include "DisplayConfig.hpp"
+#include <DisplayConfig.hpp>
 
 void setup() {
   Serial.begin(115200);
   while(!Serial) {}
-  display.init(115200, true, 20, false);
-  display.setRotation(1);
-  display.setFullWindow();
+  initialiseDisplay();
   display.firstPage();
   display.setFont(&FreeSans12pt7b);
   display.setTextColor(GxEPD_BLACK);
-}
-
-void loop() {
   do {
     display.fillScreen(GxEPD_WHITE);
     display.setCursor(25, 85);
     display.print("ESP and EPaper is easy!");
+    Serial.println("Page!");
   } while (display.nextPage());
-  display.powerOff();
-  ESP.deepSleep(0);
+  //display.powerOff();
+  Serial.println("Entering deep sleep forever.");
+  delay(1000);
+  Serial.flush();
+  esp_deep_sleep_start();
 }
+
+void loop() {} // this will never run!
