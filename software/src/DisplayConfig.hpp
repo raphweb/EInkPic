d008dc4d44d17f5227979e2104416aedc32bf420
Raphael Weber <5662800+raphweb@users.noreply.github.com>
2024-08-18 21:34:56 +0200

    Specialized Base64Format implemented.

--- software/src/DisplayConfig.hpp
+++ software/src/DisplayConfig.hpp
@@ -15,11 +15,13 @@
 #define EPD_MISO 19 // Master-In Slave-Out not used, as no data from display
 #define EPD_MOSI 23 // to EPD DIN
 
-#define MAX_DISPLAY_BUFFER_SIZE 48000UL
-//#define MAX_HEIGHT(EPD) (EPD::HEIGHT <= (MAX_DISPLAY_BUFFER_SIZE) / (EPD::WIDTH / 2) ? EPD::HEIGHT : (MAX_DISPLAY_BUFFER_SIZE) / (EPD::WIDTH / 2))
-GxEPD2_DISPLAY_CLASS<GxEPD2_DRIVER_CLASS, /*MAX_HEIGHT(GxEPD2_DRIVER_CLASS)*/1> display(GxEPD2_DRIVER_CLASS(EPD_CS, EPD_DC, EPD_RST, EPD_BUSY));
+#define MAX_DISPLAY_BUFFER_SIZE 16000UL
+#define PAGE_HEIGHT (GxEPD2_DRIVER_CLASS::HEIGHT <= (MAX_DISPLAY_BUFFER_SIZE) / (GxEPD2_DRIVER_CLASS::WIDTH / 2) ? GxEPD2_DRIVER_CLASS::HEIGHT : (MAX_DISPLAY_BUFFER_SIZE) / (GxEPD2_DRIVER_CLASS::WIDTH / 2))
+#define NUMBER_OF_PAGES (GxEPD2_DRIVER_CLASS::HEIGHT / PAGE_HEIGHT + ((GxEPD2_DRIVER_CLASS::HEIGHT % PAGE_HEIGHT) > 0))
+GxEPD2_DISPLAY_CLASS<GxEPD2_DRIVER_CLASS, /*PAGE_HEIGHT*/1> display(GxEPD2_DRIVER_CLASS(EPD_CS, EPD_DC, EPD_RST, EPD_BUSY));
 //#undef MAX_DISPLAY_BUFFER_SIZE
-//#undef MAX_HEIGHT
+//#undef PAGE_HEIGHT
+//#undef NUMBER_OF_PAGES
 
 SPIClass hspi(HSPI);
 
@@ -30,5 +32,21 @@ void initialiseDisplay() {
     display.clearScreen();
     display.setFullWindow();
 }
+
+// custom base64 map to directly map to the E-Ink 7 color schema
+const uint8_t PROGMEM base64Map[256] = {
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, /*0*/ 0b01100100, /*1*/ 0b01100101, /*2*/ 0b01100110, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    /*A*/ 0b00000000, /*B*/ 0b00000001, /*C*/ 0b00000010, /*D*/ 0b00000011, /*E*/ 0b00000100, /*F*/ 0b00000101, /*G*/ 0b00000110, 0, // no H
+    /*I*/ 0b00010000, /*J*/ 0b00010001, /*K*/ 0b00010010, /*L*/ 0b00010011, /*M*/ 0b00010100, /*N*/ 0b00010101, /*O*/ 0b00010110, 0, // no P
+    /*Q*/ 0b00100000, /*R*/ 0b00100001, /*S*/ 0b00100010, /*T*/ 0b00100011, /*U*/ 0b00100100, /*V*/ 0b00100101, /*W*/ 0b00100110, 0, // no X
+    /*Y*/ 0b00110000, /*Z*/ 0b00110001, 0, 0, 0, 0, 0, 0, /*a*/ 0b00110010, /*b*/ 0b00110011, /*c*/ 0b00110100, /*d*/ 0b00110101, /*e*/ 0b00110110, 0, // no f
+    /*g*/ 0b01000000, /*h*/ 0b01000001, /*i*/ 0b01000010, /*j*/ 0b01000011, /*k*/ 0b01000100, /*l*/ 0b01000101, /*m*/ 0b01000110, 0, // no n
+    /*o*/ 0b01010000, /*p*/ 0b01010001, /*q*/ 0b01010010, /*r*/ 0b01010011, /*s*/ 0b01010100, /*t*/ 0b01010101, /*u*/ 0b01010110, 0, // no v
+    /*w*/ 0b01100000, /*x*/ 0b01100001, /*y*/ 0b01100010, /*z*/ 0b01100011, // no 3, 4, 5, 6, 7, 8, 9, +, /
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+};
 
 #endif
--- software/src/EInkPic.cpp
+++ software/src/EInkPic.cpp
@@ -6,85 +6,72 @@
 #include <ArduinoJson.h>
 #include "config.hpp"
 #include "nosignalimage.hpp"
-#include "mbedtls/base64.h"
 
-enum state_t{
-  CONFIG_RECIEVED = 0,
-  WAIT_FOR_IMAGE = 1,
-  PART_1_REVCIEVED = 2,
-  PART_2_REVCIEVED = 3,
-  PART_3_REVCIEVED = 4,
-  PART_4_REVCIEVED = 5
+enum state_t {
+  WAIT_FOR_IMAGE  = 0,
+  CONFIG_RECIEVED = 1
 };
 
 state_t state = WAIT_FOR_IMAGE;
+uint8_t pagesReceived = 0;
 uint64_t sleep_time = DEFAULT_SLEEP_TIME;
 unsigned long last_message = 0;
 WiFiClient espClient;
 StaticJsonDocument<200> doc;
-void callback(char* topic, byte* message, unsigned int length) ;
+void callback(char* topic, byte* message, unsigned int length);
 
-PubSubClient client(mqtt_server, port,callback,espClient);
+PubSubClient client(mqtt_server, port, callback, espClient);
 
-void handle_states(byte number){
+void handle_states(byte number) {
   last_message = millis();
-  Serial.print("State handling:  ");
+  Serial.print("State handling: ");
   Serial.print(number);
-  if (state == ((int)number-48-1)) {
-    state = (state_t)(number-48);
+  if (pagesReceived == ((int)number-48-1)) {
+    pagesReceived = (state_t)(number-48);
   } else {
     Serial.print("UPS");
     state = CONFIG_RECIEVED; // will try again in 30 minutes
   }
 }
 
-void set_new_sleep_time(byte* message){
+void set_new_sleep_time(byte* message) {
   DeserializationError error = deserializeJson(doc, message);
   if (error) { //Check for errors in parsing
     Serial.println("Parsing failed");
     sleep_time = 30;
   }
   const char * image = doc["image_name"];
-  Serial.println("Sucessfully updated image to:");
+  Serial.print("Sucessfully updated image to: ");
   Serial.println(image);
   sleep_time = doc["refresh"];
-  Serial.println("Updated refresh Time to :");
+  Serial.print("Updated refresh time to: ");
   Serial.println(sleep_time);
 }
 
 uint8_t pixelBuffer[MAX_DISPLAY_BUFFER_SIZE];
 
 void convertToPanelFormat(const byte *b64, const uint8_t pageNr) {
-  uint8_t decBuf[3];
-  size_t olen;
-  for(uint16_t i = 0; i < MAX_DISPLAY_BUFFER_SIZE / 4; i++) {
-    mbedtls_base64_decode(&(decBuf[0]), 3, &olen, &(b64[i*4]), 4);
-    pixelBuffer[i*4+0] = ((decBuf[0] & 0b00000111)     ) | ((decBuf[0] & 0b00111000) << 1);
-    pixelBuffer[i*4+1] = ((decBuf[0] & 0b11000000) >> 6) | ((decBuf[1] & 0b00000001) << 2) | ((decBuf[1] & 0b00001110) << 3);
-    pixelBuffer[i*4+2] = ((decBuf[1] & 0b01110000) >> 4) | ((decBuf[1] & 0b10000000) >> 3) | ((decBuf[2] & 0b00000011) << 5);
-    pixelBuffer[i*4+3] = ((decBuf[2] & 0b00011100) >> 2) | ((decBuf[2] & 0b11100000) >> 1);
+  for(uint16_t i = 0; i < MAX_DISPLAY_BUFFER_SIZE; i++) {
+    pixelBuffer[i] = base64Map[(const uint8_t)b64[i]];
   }
-  display.writeNative(pixelBuffer, 0, 0, MAX_DISPLAY_BUFFER_SIZE / (GxEPD2_DRIVER_CLASS::WIDTH / 2) * pageNr,
-      GxEPD2_DRIVER_CLASS::WIDTH, MAX_DISPLAY_BUFFER_SIZE / (GxEPD2_DRIVER_CLASS::WIDTH / 2), false, false, false);
+  display.writeNative(pixelBuffer, 0, 0, PAGE_HEIGHT * pageNr,
+      GxEPD2_DRIVER_CLASS::WIDTH, PAGE_HEIGHT, false, false, false);
 }
 
 void callback(char* topic, byte* message, unsigned int length) {
   Serial.print("Message arrived on topic: ");
   Serial.print(topic);
   handle_states(message[9]);
-  if (state < 5) {
-    //display.copyToRawPixelBuffer(&(message[21]));
-    if (state != PART_4_REVCIEVED) display.nextPage(); //4. slides will cause the break of the callback
-    client.publish(("Eink_frame/" + String(name) + "/ack").c_str(), String(state).c_str());
-  } else {
+  convertToPanelFormat(&(message[21]), pagesReceived);
+  if (pagesReceived >= NUMBER_OF_PAGES) {
     set_new_sleep_time(message);
-    client.publish(("Eink_frame/" + String(name) + "/ack").c_str(), String(state).c_str());
   }
+  client.publish(("Eink_frame/" + String(name) + "/ack").c_str(), String(state).c_str());
 }
 
 void enterSleepMode(boolean displayDefaultPicture = false) {
   if (displayDefaultPicture) {
-    for (uint8_t i = 0; i < 4; i++) {
+    for (uint8_t i = 0; i < NUMBER_OF_PAGES; i++) {
       convertToPanelFormat((const byte*)&testPic[MAX_DISPLAY_BUFFER_SIZE*i], i);
     }
   }
@@ -110,19 +97,19 @@ void setup() {
       enterSleepMode(true);
     }
   }
-  client.setBufferSize(50000);
+  client.setBufferSize(20000);
   if (client.connect(name, user, pswd)) {
     client.subscribe(("Eink_frame/" + String(name) + "/send_image").c_str());
   }
   client.publish(("Eink_frame/" + String(name) + "/get_image").c_str(), "hi_guys"); 
   last_message = millis();
-  while (state > CONFIG_RECIEVED) {
+  while (state < CONFIG_RECIEVED) {
     client.loop();
     if (millis() - last_message > TIMEOUT_MQTT_MS) {
       enterSleepMode(true);
     }
   }
-  display.nextPage(); // Needed to display the last slice after successfully receiving all parts
+  //display.nextPage(); // Needed to display the last slice after successfully receiving all parts
   enterSleepMode();
 }
 
